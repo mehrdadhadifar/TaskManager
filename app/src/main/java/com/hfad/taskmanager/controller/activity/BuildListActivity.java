3019e058110a5e934c1a023b8dae5761e76596db
mehrdadhadifar <mlp8512@gmail.com>
2020-08-01 02:47:03 +0430

    Create BuildTaskActivity and Fragment

---
+++ app/src/main/java/com/hfad/taskmanager/controller/activity/BuildListActivity.java
@@ -0,0 +1,16 @@
+package com.hfad.taskmanager.controller.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hfad.taskmanager.R;
+
+public class BuildListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_build_list);
+    }
+}
